2af60bf8e8c699fca5299585ac1c96fa3102fcb1
Anne Quiedeville <anne.quiedeville@orange.fr>
2019-09-19 18:36:49 +0200

    ajout menu burger responsive et navbar page 2

--- script.js
+++ script.js
@@ -2,3 +2,12 @@ function confirm() {
     const message = 'Commande validée, retour à la page 1';
     alert(message);
 }
+
+function burgerResponsive() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "navbar") {
+        x.className += " responsive";
+    } else {
+        x.className = "navbar";
+    }
+} 
